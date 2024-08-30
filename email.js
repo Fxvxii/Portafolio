7e046eb4cfe58e31492b06d4d8156fe62dd5c507
Fxvxii <mafer23.2c@gmail.com>
2024-08-30 11:52:20 -0600

    Portafolio

---
+++ email.js
@@ -0,0 +1,45 @@
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_72s7r9i';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
+
+
+const faqHeads = document.querySelectorAll(".faq .head");
+
+faqHeads.forEach((faqHead) => {
+    // Asignamos el evento click a cada .head
+    faqHead.addEventListener("click", () => {
+        // Obtenemos el nodo padre del .head, es decir .faq
+        const faq = faqHead.parentNode;
+        // Obtenemos el elemento contiguo al .head, es decir .content
+        const faqContent = faqHead.nextElementSibling;
+        
+        // toggle() permite quitar o asignar la clase "active" al .faq
+        faq.classList.toggle("active");
+        
+        // Si faq tiene la clase "active" entonces asignas al .content una altura
+        // esta altura es el alto del contenido que esta oculto + 30px
+        if (faq.classList.contains("active")) {
+            faqContent.style.height = (faqContent.scrollHeight + 30) + "px";
+        } else {
+            // caso contrario, tendrá una altura de 0px
+            faqContent.style.height = "0px";
+        }
+    });
+});
